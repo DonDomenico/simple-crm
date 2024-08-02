994f3c4de9560faea1acb7e207e00d6239548dbf
DonDomenico <d.grunow@hotmail.de>
2024-08-02 15:21:55 +0200

    added user class

--- src/app/add-user-dialog/add-user-dialog.component.ts
+++ src/app/add-user-dialog/add-user-dialog.component.ts
@@ -5,25 +5,44 @@ import {
   MatDialogRef,
   MatDialogClose
 } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormsModule, 
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-user-dialog',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatInputModule, MatDialogClose, MatButtonModule, FormsModule, MatFormFieldModule, MatDatepickerModule],
+  imports: [MatDialogContent, MatDialogActions, MatInputModule, MatDialogClose, MatButtonModule, FormsModule, MatFormFieldModule, MatDatepickerModule, ReactiveFormsModule],
   templateUrl: './add-user-dialog.component.html',
   styleUrl: './add-user-dialog.component.scss',
   providers: [provideNativeDateAdapter()]
 })
 export class AddUserDialogComponent {
-  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) {}
+  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) { }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 }
---
+++ src/app/models/user.class.ts
@@ -0,0 +1,19 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    birthday: number;
+    street: string;
+    zipCode: number;
+    city: string
+
+    constructor(obj?: any) {
+        this.firstName = obj ? obj.firstName : '';
+        this.lastName = obj ? obj.lastName : '';
+        this.email = obj ? obj.email : '';
+        this.birthday = obj ? obj.birthday : '';
+        this.street = obj ? obj.street : '';
+        this.zipCode = obj ? obj.zipCode : '';
+        this.city = obj ? obj.city : '';
+    }
+}
