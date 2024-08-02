e20d1d8ee3eb7028c03421cc0b180cbf626abc60
DonDomenico <60230746+DonDomenico@users.noreply.github.com>
2024-08-02 15:55:39 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,5 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-649fc","appId":"1:1017144676391:web:7d22288659222a7e767e02","storageBucket":"simple-crm-649fc.appspot.com","apiKey":"AIzaSyCWIDwR6FnBoDKu8xorqulQCrTACiduAVw","authDomain":"simple-crm-649fc.firebaseapp.com","messagingSenderId":"1017144676391"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
