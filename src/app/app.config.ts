3c02efc5976547c62eb0443560497fef1a5194c5
DonDomenico <d.grunow@hotmail.de>
2024-08-06 17:02:51 +0200

    structural changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from '../environments/environment/environment';
+import { firebaseConfig } from './firebase/firebase-credentials';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -8,6 +8,9 @@ import {
 } from '@angular/material/dialog';
 import { AddUserDialogComponent } from '../add-user-dialog/add-user-dialog.component';
 import { MatCardModule } from '@angular/material/card';
+import { doc, onSnapshot } from "firebase/firestore";
+import { collection, Firestore } from '@angular/fire/firestore';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -17,10 +20,27 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  user = new User();
+  firestore = inject(Firestore);
+  users: User[] = [];
+  unsubUserList;
+  // unsubSingleUser;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {
+    this.unsubUserList = this.subUserList();
+
+    // this.unsubUserList();
+  }
+
+  subUserList() {
+    return onSnapshot(collection(this.firestore, 'users'), list => {
+      list.forEach(element => {
+        this.users.push();
+      })
+    })
+  }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(AddUserDialogComponent)
+    const dialogRef = this.dialog.open(AddUserDialogComponent);
   }
 }
