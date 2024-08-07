c7033884d2313b5624b35bb08cc9cd3814281924
DonDomenico <d.grunow@hotmail.de>
2024-08-07 14:44:30 +0200

    moved function toJson to firebase-service

--- src/app/firebase-services.service.ts
+++ src/app/firebase-services.service.ts
@@ -19,23 +19,24 @@ export class FirebaseServicesService {
   constructor() {
     this.unsubUserList = this.subUserList();
 
+  }
+
+  ngOnDestroy() {
     // this.unsubUserList();
   }
 
   subUserList() {
     return onSnapshot(this.getUserRef(), list => {
       list.forEach(element => {
-        this.users.push();
+        console.log(this.toJson(element.data(), element.id));
       })
     })
   }
 
   async saveUser() {
     this.loading = true;
-    if (this.user.birthday != null) {
-      this.user.birthday = this.birthday.getTime();
-    }
-    await addDoc(this.getUserRef(), this.user.toJson()).catch(
+    this.user.birthday = this.birthday.getTime();
+    await addDoc(this.getUserRef(), this.toJson(this.user)).catch(
       (err) => { console.error(err) }
     ).then(
       () => {
@@ -51,5 +52,18 @@ export class FirebaseServicesService {
 
   getSingleUserRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
+  }
+
+  toJson(obj: any, id?: string): User {
+    return {
+        id: id || "",
+        firstName: obj.firstName || "",
+        lastName: obj.lastName || "",
+        email: obj.email || "",
+        birthday: obj.birthday || 0,
+        street: obj.street || "",
+        zipCode: obj.zipCode || 0,
+        city: obj.city || ""
+    }
   }
 }
